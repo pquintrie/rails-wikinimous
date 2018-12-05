6f4c80266d68ca84e95140b2a7273d6f99782fab
Paul Quintrie Lamothe <paul.quintrie.lamothe@hotmail.fr>
2018-12-05 18:32:51 +0100

    initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,19 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+
+puts 'Creating 10 fake articles...'
+10.times do
+    article = Article.new(
+    title:    Faker::Book.title,
+    content: Faker::Movies::BackToTheFuture.quote
+  )
+  article.save!
+end
+puts 'Finished!'
